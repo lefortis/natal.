1e923b4227a71869c4b855ed54dcf55caf2d6316
lefortis <leticiagabrielasgomes@gmail.com>
2024-12-23 02:27:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirCartinha() {
+    
+    const cartinha = document.getElementById("cartinha");
+    cartinha.style.display = "block";
+
+
+    const envelope = document.querySelector(".envelope");
+    envelope.style.display = "none";
+}
